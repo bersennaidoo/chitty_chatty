629858a6af22a64876b720c7a6ee8af7a179c6fe
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-03-13 12:31:05 +0200

    Completed user signup featured and implemented end to end testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
-      # 
+      flash[:success] = "Welcome to the Chitty Chatty App!"
+      redirect_to @user
     else
       render "new", status: :unprocessable_entity
     end
