331de92c0f52d713d50cb7e70c92487accd8c782
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-03-15 12:14:00 +0200

    Completed login and logout feature using sessions and implemented tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
+      reset_session
+      log_in @user
       flash[:success] = "Welcome to the Chitty Chatty App!"
       redirect_to @user
     else
