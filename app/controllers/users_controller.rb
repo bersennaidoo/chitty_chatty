e36fc6b6a0648166ca8e35a0f0f84f6fa4ecb665
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-03-19 13:06:06 +0200

    Completed users edit profile feature and tested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,16 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to user_path(@user)
+    else
+      render "edit", status: :unprocessable_entity
+    end
+  end
   
   private def user_params
     params.require(:user).permit(:name, :email, 
